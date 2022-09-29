cde1648d81282bf776d67da354e659d1c7984c42
py <py.boulbin@openfly.fr>
2022-09-29 17:33:05 +0200

    update serve exemple

--- dev/serve.vue
+++ dev/serve.vue
@@ -9,11 +9,38 @@ export default Vue.extend({
 	components: {
 		CookiesConsent,
 	},
+	data() {
+		return {
+            showSnackbar: false,
+            preferences: [
+                {
+                    title: 'Analytics',
+                    description:
+						'Bla bla serviços que podemos oferecer erviços que podemos oferecer erviços que podemos oferecer erviços que podemos oferecer serviços que podemos oferecer.',
+                    items: [
+                        {
+                            label: 'GoogleAnalytics',
+                            value: 'ga',
+                            isEnable: true,
+                        },
+                        { label: 'Sentry', value: 'sentry', isEnable: true },
+                        { label: 'Mapbox', value: 'mapbox', isEnable: true },
+                        {
+                            label: 'New Relic',
+                            value: 'newRelic',
+                            isEnable: true,
+                        },
+                        { label: 'Dog Food', value: 'dogfood', isEnable: true },
+                    ],
+                },
+            ],
+        };
+	}
 });
 </script>
 
 <template>
 	<div id="app">
-		<cookies-consent />
+		<cookies-consent :preferences="preferences" />
 	</div>
 </template>
